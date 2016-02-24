c5defb66c82e7309cec5bb900aaeee38c8adee86
Nicolai Czempin <nicolai.czempin@gmail.com>
2016-02-24 11:36:13 +0100

    Clean up compiler warnings

--- ls.c
+++ ls.c
@@ -11,6 +11,7 @@
 #include <sys/syscall.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <string.h>
 #define BUFSZ 1024
 
@@ -45,12 +46,11 @@ int main()
 	struct stat st;
 	struct passwd *pw;
 	struct group *grp;
-	char buf[BUFSZ], *bp, *ftime;
+	char buf[BUFSZ], *bp;
 	char buf2[160];
-	int dfd, fd, nread;
+	int dfd, nread;
 	struct linux_dirent *d;
 	int bpos;
-	char d_type;
 
 	dfd = open(".", O_RDONLY|O_DIRECTORY);
 	if (dfd == -1)
@@ -73,27 +73,22 @@ int main()
 			dir = (struct linux_dirent *)buf;
 			if (dir->d_reclen != 0) {
 				stat(d->d_name, &st);
-				ftime = ctime_r(&st.st_mtime,buf2);
+				ctime_r(&st.st_mtime,buf2);
 				buf2[16]='\0';
 				pw = getpwuid(st.st_uid);
 				grp = getgrgid(st.st_gid);
 				perms(st.st_mode);
-				printf("%3d ",st.st_nlink);
+				printf("%3d ", (int)st.st_nlink);
 				if (pw!=0){
-					printf("%-8s ",pw->pw_name);
+					printf("%-8s ", pw->pw_name);
 				}
 				if (grp!=0) 
 				{
 					printf("%-8s ", grp->gr_name);
 				}
-				printf("%9d ", st.st_size);
-				printf("%s ",&buf2[4]);
-				printf("%s\n",d->d_name);
-				/*
-					 printf("%3d %-8s %-7s %9d %s %s\n",
-					 st.st_nlink, pw->pw_name, grp->gr_name,    
-					 st.st_size, ftime, dir->d_name);
-					 */
+				printf("%9d ", (int)st.st_size);
+				printf("%s ", &buf2[4]);
+				printf("%s\n", d->d_name);
 			}
 			bp = bp + dir->d_reclen;
 			dir = (struct linux_dirent *)(bp);
